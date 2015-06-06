1358f1d75c2df19c0db0eed5941cd36e474146ad
lbohl <lbohl@expedia.com>
2015-06-06 14:14:52 +0100

    add javadoc

--- src/main/java/com/hotels/cascading/fields/FieldsBaseOperation.java
+++ src/main/java/com/hotels/cascading/fields/FieldsBaseOperation.java
@@ -3,12 +3,16 @@ package com.hotels.cascading.fields;
 import java.io.Serializable;
 import java.lang.reflect.Type;
 
+/**
+ * Default implementation of {@link FieldsFunction}
+ */
 public abstract class FieldsBaseOperation<R> implements FieldsFunction<R>, Serializable {
 
   private int pos;
   private String declaredFieldName;
   private Type declaredType;
 
+  @Override
   public void prepare(int pos, String declaredFieldName, Type declaredType) {
     this.pos = pos;
     this.declaredFieldName = declaredFieldName;
