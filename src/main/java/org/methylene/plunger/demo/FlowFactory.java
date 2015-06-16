fe17a0b4b9ffd532f3bfae974f5b053643a3c302
lbohl <lbohl@expedia.com>
2015-06-16 13:48:52 +0100

    add solution for task_revenue_usd

--- src/main/java/org/methylene/plunger/demo/FlowFactory.java
+++ src/main/java/org/methylene/plunger/demo/FlowFactory.java
@@ -3,6 +3,7 @@ package org.methylene.plunger.demo;
 import cascading.flow.Flow;
 import cascading.flow.FlowConnector;
 import cascading.flow.FlowDef;
+import cascading.pipe.Each;
 import cascading.pipe.HashJoin;
 import cascading.pipe.Pipe;
 import cascading.pipe.assembly.Retain;
@@ -19,6 +20,8 @@ public class FlowFactory {
   public static final Fields CURRENCY = new Fields("currency", String.class);
   public static final Fields FACTOR = new Fields("factor", Double.TYPE);
   public static final Fields TMP_CURRENCY = new Fields("tmp_currency", String.class);
+  public static final Fields REVENUE_USD = new Fields("revenue_usd", Double.TYPE);
+
   final Tap revenue;
   final Tap conversion;
   final Tap sink;
@@ -37,6 +40,7 @@ public class FlowFactory {
     Pipe joinPipe = new HashJoin(revenuePipe, CURRENCY, conversionPipe, CURRENCY,
         Fields.join(REVENUE, CURRENCY, TMP_CURRENCY, FACTOR), new LeftJoin());
     joinPipe = new Retain(joinPipe, Fields.join(REVENUE, CURRENCY, FACTOR));
+    joinPipe = new Each(joinPipe, new Multiply(REVENUE_USD, REVENUE, FACTOR), Fields.ALL);
 
     // bind taps to pipes
     FlowDef flowDef = new FlowDef()
